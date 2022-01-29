968cbfab96fee7ec49921fef7e6ded9f86033818
annnnnnna <annnnnnnnnnnnna@gmail.com>
2022-01-29 13:08:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 function makeQuestion(random) {
-    getOrigin(random, '');
+    getOrigin(random, '', '');
 }
-function getOrigin(random, param) {
+function getOrigin(random, param, param2) {
     if (param == '') {
-        param = "&pageids=" + random.nextInt(1, 4500000);
+        param = '&generator=random&grnnamespace=0';
+//        param = "&pageids=" + random.nextInt();
     }
     $.getJSON("https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=links&pllimit=500&redirects=true&origin=*" + param, function (json){
         let title = '';
         for (const page in json.query.pages) {
             if (json.query.pages[page].title && json.query.pages[page].ns == 0) {
+                $('#orig_id')[0].value = json.query.pages[page].pageid;
                 title = json.query.pages[page].title;
-                $('#origin')[0].value = title;
+                $('#origin')[0].innerText = title;
                 for (const link in json.query.pages[page].links) {
                     if (json.query.pages[page].links[link].ns == 0) {
                         $('#nextList').append($('<option>').html(json.query.pages[page].links[link].title).val(json.query.pages[page].links[link].title));
@@ -19,27 +21,33 @@ function getOrigin(random, param) {
             }
         }
         if (title == '') {
-            getOrigin(random, '');
+            getOrigin(random, '', param2);
         } else if (json.continue && json.continue.plcontinue) {
-            getOrigin(random, param + '&plcontinue=' + json.continue.plcontinue);
+            getOrigin(random, '&pageids=' + $('#orig_id')[0].value + '&plcontinue=' + json.continue.plcontinue, param2);
         } else {
-            getDestination(random);
+            getDestination(random, param2);
         }
     });
 }
 
-function getDestination(random) {
-    let param = "&pageids=" + random.nextInt(1, 4500000);
-    $.getJSON("https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=links&pllimit=1&redirects=true&origin=*"+param, function (json){
+function getDestination(random, param) {
+    if (param == '') {
+        param = '&generator=random&grnnamespace=0';
+//        param = "&pageids=" + random.nextInt();
+    }
+    $.getJSON("https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&redirects=true&origin=*&rvprop=content&rvslots=*"+param, function (json){
         let title = '';
-	for (const page in json.query.pages) {
+        for (const page in json.query.pages) {
             if (json.query.pages[page].title && json.query.pages[page].ns == 0) {
+                $('#dest_id')[0].value = json.query.pages[page].pageid;
                 title = json.query.pages[page].title;
-                $('#destination')[0].value = title;
+                $('#destination')[0].innerText = title;
+                let dom = new DOMParser().parseFromString('<div>' + json.query.pages[page].extract + '</div>', "text/xml");
+                $('#popup_description')[0].innerText = dom.getElementsByTagName('p')[0].textContent;
             }
-	}
+        }
         if (title == '') {
-            getDestination(random);
+            getDestination(random, '');
         }
     });
 }
@@ -58,25 +66,27 @@ function getLinksByTitle(title, plcontinue){
         }
     });
 }
+
 function today() {
     const today = new Date();
-    const seed = ("" + today.getFullYear() + (today.getMonth()+10) + today.getDate()) * 1;
+    const seed = ("" + today.getFullYear() + (today.getMonth()+10) + today.getDate()+10) * 1;
     const random = new Random(seed);
-    if ($('#origin')[0].value != seed) {
+    if ($('#seed')[0].value != seed) {
         makeQuestion(random);
         $('#seed')[0].value = seed;
+    } else {
+        getOrigin(random, '&pageids=' + $('#orig_id')[0].value, '&pageids=' + $('#dest_id')[0].value);
     }
 }
 
 class Random {
-  constructor(seed = 88675123) {
+  constructor(seed) {
     this.x = 123456789;
     this.y = 362436069;
     this.z = 521288629;
     this.w = seed;
   }
 
-  // XorShift
   next() {
     const t = this.x ^ (this.x << 11);
     this.x = this.y;
@@ -85,22 +95,36 @@ class Random {
     return this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8));
   }
 
-  // min 以上 max 以下の乱数を生成する
-  nextInt(min, max) {
+  nextInt() {
     const r = Math.abs(this.next());
-    return min + (r % (max + 1 - min));
+    return 1 + (r % 4500000);
   }
 }
+
 function goal() {
     alert('GOAL!');
 }
+
 function shot() {
     const selectItem = $('#nextList').val();
     $('#nextList > option').remove();
     if (selectItem == $('#destination')[0].value) {
         goal();
     } else {
-        $('#prevList').append($('<option>').html(selectItem).val(selectItem));
+        $('#prevList').append($('<li>').html(selectItem));
         getLinksByTitle(selectItem, '');
     }
 }
+
+function popupDescription() {
+  if(!$("#popup")) return;
+  closePopUp($("#popup")[0]);
+  closePopUp($("#question")[0]);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      $("#popup")[0].classList.toggle('is-show');
+    });
+  }
+}
+
